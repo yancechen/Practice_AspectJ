cef94757a882a2f8a99fb275976a9fbb15da6c0e
chenyouyu <chenyouyu@58.com>
2023-03-21 09:45:21 +0800

    init project

---
+++ app/src/main/java/com/example/practice_aspectj/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.practice_aspectj;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
